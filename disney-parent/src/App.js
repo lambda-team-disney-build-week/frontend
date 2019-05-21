d1d53bd609f11153bab9d873919e0cd88d45d307
Frank Martinez <frankie.martinez@gmail.com>
2019-05-21 15:31:58 -0700

    catching commit. Added reactstrap names, housekeeping

--- disney-parent/src/App.js
+++ disney-parent/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import Login from './pages/Login';
-import {Route, Switch} from 'react-router-dom';
-import Auth from './components/Auth';
+import Login from './components/Login';
+import { Route, BrowserRouter } from 'react-router-dom';
+// import Auth from './components/Auth';
 import Home from './pages/Home';
-import RequestForm from './pages/RequestForm';
+// import RequestForm from './pages/RequestForm';
 
 
 class App extends Component {
@@ -26,14 +26,14 @@ class App extends Component {
   }
   render() {
     return (
-      <Switch>
+      <BrowserRouter >
         <Route path ='/' exact component={Home}></Route>
         <Route path ='/login' component ={Login}></Route>
-        <Auth>
+        {/* <Auth>
         <Route path ='/Parent' component ={Posts}></Route>
         <Route path ='/request' component={RequestForm}></Route>
-        </Auth>
-      </Switch>
+        </Auth> */}
+      </BrowserRouter >
     );
   }
 }
--- disney-parent/src/components/Jumbo.js
+++ disney-parent/src/components/Jumbo.js
@@ -2,18 +2,18 @@ import React, {Component} from 'react';
 import './jumbo.css';
 import {Button, Jumbotron} from 'reactstrap';
 
-class Jumbotron extends Component {
+class Jumbo extends Component {
     render(){
         return(
             
             <div className="jumbotron">
-            <Jumbotron>
-                <h1 className="display-4">Disney Parent</h1>
-                {this.props.loginForm ? 
-                    <Button onClick={this.props.toggle} className="btn btn-lg btn-blue">Sign Up</Button>
-                        :
-                    <Button onClick={this.props.toggle} className="btn btn-lg btn-blue">Sign In</Button>}
-            </Jumbotron>
+                <Jumbotron>
+                    <h1 className="display-4">Disney Parent</h1>
+                    {this.props.loginForm ? 
+                        <Button onClick={this.props.toggle} className="btn btn-lg btn-blue">Sign Up</Button>
+                            :
+                        <Button onClick={this.props.toggle} className="btn btn-lg btn-blue">Sign In</Button>}
+                </Jumbotron>
             </ div>
             
         
@@ -22,4 +22,4 @@ class Jumbotron extends Component {
     }
 }
 
-export default Jumbotron
+export default Jumbo

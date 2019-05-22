7c0bcda56b0c396d5dd1e64a6ddde0c773ba3be5
Manya0 <manya01@protonmail.com>
2019-05-22 13:47:35 -0400

    Posts-file-not rendering

---
+++ disney-parent/src/pages/Posts.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import Post from './Post';
+import Nav from './Nav';
+
+export default class Posts extends Component {
+    constructor(props){
+        super(props)
+    this.state= {
+        posts: [],
+        users: []
+        }
+    }
+
+    componentDidMount(){
+        this.getPosts();
+        this.getUsers();
+    }
+
+    getPosts = () => {
+        const token = localStorage.getItem('token')
+    axios
+      .get('https://disneyparent-backend.herokuapp.com/posts', {headers: {Authorization: token}})
+      .then(res=> 
+        {console.log(res.data)
+        this.setState({      
+            posts: res.data
+        })
+        console.log(this.state.posts)
+    })
+    .catch(err=> console.log(err))
+    }
+
+    getUsers = () => {
+        const token = localStorage.getItem('token')
+        axios
+          .get('https://disneyparent-backend.herokuapp.com/parents', {headers: {Authorization: token}})
+          .then(res=> 
+           {console.log(res.data)
+           this.setState({      
+           users: res.data
+           })
+           console.log(this.state.users)
+          })
+          .catch(err=> console.log(err))        
+    }
+     render(){
+      return(
+          <div>
+              <Nav />s
+              {this.state.posts.map((post)=>
+                <Post post={post} key={post.id} getPosts={this.getPosts}/>
+              )}
+          </div>
+      )
+  }
+}    
